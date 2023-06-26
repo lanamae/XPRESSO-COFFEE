b3a90ef7e80dc64439929d8f39626a23038cf621
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2023-06-26 23:28:23 +0800

    added tablet.scss

---
+++ Source-Code/navbar.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.close');
+const navbar = document.querySelector('#navbar');
+
+
+// close.addEventListener('click', () =>{
+//     navbar.classList.remove = 'open';
+// })
+
+// menu.addEventListener('click', () =>{
+//     navbar.classList.add = 'open';
+//     // alert('test')
+    
+// })
+
+const closeNav = () =>{
+    navbar.classList.remove = 'open';
+}
+
+const openNav = () =>{
+    navbar.classList.add = 'open';
+}
