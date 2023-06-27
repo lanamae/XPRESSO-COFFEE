09dbe084807f6f69b3e479e78284c87bec1b59a7
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2023-06-27 12:53:10 +0800

    Modify mobile and tablet-navbar and hero section

--- Source-Code/navbar.js
+++ Source-Code/navbar.js
@@ -3,20 +3,22 @@ const close = document.querySelector('.close');
 const navbar = document.querySelector('#navbar');
 
 
-// close.addEventListener('click', () =>{
-//     navbar.classList.remove = 'open';
-// })
+close.addEventListener('click', () =>{
+    navbar.classList.remove('open');
+})
 
-// menu.addEventListener('click', () =>{
-//     navbar.classList.add = 'open';
-//     // alert('test')
+menu.addEventListener('click', () =>{
+    navbar.classList.add('open');
+    // alert('test')
     
-// })
+})
 
-const closeNav = () =>{
-    navbar.classList.remove = 'open';
-}
 
-const openNav = () =>{
-    navbar.classList.add = 'open';
-}
+// const closeNav = () =>{
+//     navbar.classList.remove('open');
+// }
+
+// const openNav = () =>{
+//     navbar.classList.add('open');
+//     alert('test')
+// }
