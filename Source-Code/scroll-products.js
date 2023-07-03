95d83638a705fbec8019f14ea93748c83f4fb911
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2023-07-03 14:26:45 +0800

    Modify the carousel drag in product in mobile

--- Source-Code/scroll-products.js
+++ Source-Code/scroll-products.js
@@ -1,26 +1,58 @@
 console.log('heyyy')
 
-// const slider = document.querySelector('.slider')
-// const productItems = document.querySelector('.product-items')
+const slider = document.querySelector('.slider')
+const productItems = document.querySelector('.product-items')
 
-// let pressed = false
-// let startx;
-// let x;
+let pressed = false
+let startx;
+let x;
 
-// slider.addEventListener('mousedown', (e) =>{
-//     pressed = true
-//     startx = e.offsetX
-//     console.log(startx)
+slider.addEventListener('mousedown', (e) =>{
+    pressed = true
+    startx = e.offsetX - productItems.offsetLeft
+    slider.style.cursor = 'grabbing'
+    console.log(productItems.offsetLeft)
+})
+
+slider.addEventListener('mouseenter', () =>{
+    slider.style.cursor = 'grab'
+    // alert('heyr34315')
+})
+
+// slider.addEventListener('mouseleave', () =>{
+//     slider.style.cursor = 'default'
 // })
 
-// slider.addEventListener('mouseenter', () =>{
-//     slider.cursor.pointer = 'grabbing'
-// })
+slider.addEventListener('mouseup', () =>{
+    slider.style.cursor = 'grab'
+})
 
-// slider.addEventListener('mouseup', () =>{
-     
-// })
+window.addEventListener('mouseup', () =>{
+    pressed = false
+})
 
-// slider.addEventListener('mousemove', () =>{
-     
-// })
+
+slider.addEventListener('mousemove', (e) =>{
+    if(!pressed) return
+    e.preventDefault()
+
+    x = e.offsetX
+
+
+    productItems.style.left=`${x - startx}px`
+})
+
+function checkBoundary (){
+    let outer = slider.getBoundingCXlientRect()
+    let inner = slider.getBoundingCXlientRect()
+
+    if(parseInt(productItems.style.left)>0){
+        productItems.style.left = '0px';
+    }
+
+    else if(inner.right<outer.right){
+        productItems.stryle.left = `-${inner.width-outer.width}px`
+    }
+}
+
+checkBoundary
